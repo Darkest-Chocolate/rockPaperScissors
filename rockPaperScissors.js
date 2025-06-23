a7cc4a35fcf9af63dc0a7fe83c09a47bed58f6c8
Darkest-Chocolate <hamuakamps@gmail.com>
2025-06-23 17:07:36 +0100

    Created Function to getHumanChoice

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,12 +4,14 @@ const rock=0;
 const paper=1;
 const scissors=2;
 
-
+let choice =("rock","paper","scissors");
 
 function getComputerChoice(){
     return Math.floor(Math.random(3))
     }
 
  
-
+function getHumanChoice(){
+    return prompt("What is your choice warrior?");
+}
     
