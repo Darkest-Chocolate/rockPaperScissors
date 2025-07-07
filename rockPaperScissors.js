0ed65b30f1cdbad5605fbe311fba5686736248c1
Darkest-Chocolate <hamuakamps@gmail.com>
2025-07-07 16:15:15 +0100

    Modified rockPaperScissors.js global scope functions

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -8,30 +8,7 @@ const rock=0;
 const paper=1;
 const scissors=2;
 
-function playRound(humanChoice,computerChoice){
-    if(playRound(rock,paper)){
-        alert("You lose, paper beats rock");
-    }else if(paper,scissors){
-        alert("You lose, scissors beats paper");
-    }else if (scissors,rock){
-        alert("You lose,rock betas scissors");
-    }else if ((paper,paper)||(rock,rock)||(scissors,scissors)){
-        alert("No wayyyy,it's a draw!");
-    }else{
-        alert("You won!You are very lucky, death to skynet!!")
-    }
 
-    
-   if (humanChoice.toLowerCase()=("rock"||"paper"||"scissors")){
-        alert (humanChoice); 
-   }
-
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
 
 let choice =("rock","paper","scissors");
 
@@ -42,12 +19,36 @@ function getComputerChoice(){
  
 function getHumanChoice(){
     var getHumanChoiceValue = prompt("What is your choice warrior?")
-        if (getHumanChoiceValue= "rock"){
-            alert("So you choose Rock!")
-        }else if (getHumanChoiceValue= "paper"){
-            alert("So you choose Paper!")
+        if (getHumanChoiceValue=="rock"){
+            alert("So you choose Rock!");
+        }else if (getHumanChoiceValue=="paper"){
+            alert("So you choose Paper!");
         }else{
-            alert("So you choose Scissors!")
+            alert("So you choose Scissors!");
 }
 }
-    
+
+
+
+
+function playRound(humanChoice,computerChoice){
+    if(rock,paper){
+        alert("You lose, paper beats rock");
+    }else if(paper,scissors){
+        alert("You lose, scissors beats paper");
+    }else if (scissors,rock){
+        alert("You lose,rock betas scissors");
+    }else if ((paper,paper)||(rock,rock)||(scissors,scissors)){
+        alert("No wayyyy,it's a draw!");
+    }else{
+        alert("You won!You are very lucky, death to skynet!!");
+    }
+
+    humanChoice = ("rock")||("paper")||("scissors");
+        return humanChoice.toLowerCase(); 
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
