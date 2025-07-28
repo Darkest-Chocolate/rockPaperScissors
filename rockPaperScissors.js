4ba9150eb9a56ce694773140c1d0bc8c6f970eaf
Darkest-Chocolate <hamuakamps@gmail.com>
2025-07-28 18:19:29 +0100

    Added if statement to getComputerChoice

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -8,55 +8,48 @@ const rock=0;
 const paper=1;
 const scissors=2;
 
-
-
-let choice =("rock","paper","scissors");
-
 function getComputerChoice(){
-    let getComputerChoiceValue= getComputerChoice
+    var getComputerChoiceValue 
         if (getComputerChoiceValue <= 0){
             getComputerChoiceValue== ("rock")
-        }else if (getComputerChoiceValue =>1 && <2){
+        }else if (getComputerChoiceValue =>1){
             getComputerChoiceValue=="paper"
         }else{
             getComputerChoiceValue=="scissors";
-
-    return Math.floor(Math.random(3))
+    return Math.floor(Math.random(4))
     }
 }
 
- 
 function getHumanChoice(){
     var getHumanChoiceValue = prompt("What is your choice warrior?")
         if (getHumanChoiceValue=="rock"){
-            alert("So you choose Rock!");
+            alert("So you choose Rock!")
         }else if (getHumanChoiceValue=="paper"){
-            alert("So you choose Paper!");
+            alert("So you choose Paper!")
         }else{
             alert("So you choose Scissors!");
 }
 }
 
-
-
-
 function playRound(humanChoice,computerChoice){
-    if(rock,paper){
-        alert("You lose, paper beats rock")
-    }else if(paper,scissors){
-        alert("You lose, scissors beats paper")
-    }else if (scissors,rock){
-        alert("You lose,rock betas scissors")
-    }else if ((paper,paper)||(rock,rock)||(scissors,scissors)){
-        alert("No wayyyy,it's a draw!")
-    }else{
-        alert("You won!You are very lucky, death to skynet!!");
-    
+
+        switch(humanChoice,computerChoice) {
+            case ("rock","paper"):
+               "You lose, paper beats rock";
+                break;
+            case "paper","scissors": "You lose, scissors beats paper";
+                break;
+            case "scissors","rock": "You lose,rock betas scissors";
+                break;
+            default : "No wayyyy,it's a draw!"; 
+                break;
+        }
     }
-    computerChoice = computerSelection
-    humanChoice = ("rock")||("paper")||("scissors")
-        return humanChoice.toLowerCase(); 
-}
+    
+
+   let humanChoice = ("rock")||("paper")||("scissors")
+         humanChoice.toLowerCase(); 
+
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
