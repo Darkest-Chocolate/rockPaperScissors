dbd836c77cbe5a52dbb953b8fd8a2b9f6af7f25c
Darkest-Chocolate <hamuakamps@gmail.com>
2025-09-08 08:28:47 +0100

    Repaired playround function to remove double prompt

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -5,39 +5,15 @@ let humanScore =0;
 let computerScore = 0;
 
 
-function playRound(humanChoice,computerChoice){
-    humanChoice =getHumanChoice();
-    humanChoice =humanChoice.toLowerCase();
-        switch(humanChoice,computerChoice) {
-            case ("rock","paper"):
-               console.log("You lose, paper beats rock");
-                break;
-            case "paper","scissors": 
-                console.log("You lose, scissors beats paper");
-                break;
-            case "scissors","rock": 
-                console.log("You lose,rock betas scissors");
-                break;
-            default : 
-                console.log("No wayyyy,it's a draw!");
-        }
+
+
+function getComputerChoice(){
+   const getComputerChoiceValue =["rock","paper","scissors"];
+    const computerValueIndex = Math.floor(Math.random()*3);
+    return getComputerChoiceValue[computerValueIndex];
     }
     
 
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
-const rock=0;
-const paper=1;
-const scissors=2;
-
-function getComputerChoice(){
-   
-    return Math.floor(Math.random(3))
-    }
 
 function getHumanChoice(){
     var getHumanChoiceValue = prompt("What is your choice warrior?")
@@ -49,3 +25,50 @@ function getHumanChoice(){
             alert("So you choose Scissors!");
 }
 }
+
+function playRound(humanChoice,computerChoice){
+        switch(humanChoice,computerChoice) {
+            case ("rock","paper"):
+               console.log("You lose, paper beats rock");
+                break;
+            case "paper","scissors": 
+                console.log("You lose, scissors beats paper");
+                break;
+            case "scissors","rock": 
+                console.log("You lose,rock beats scissors");
+                break;
+
+
+            case ("paper","rock"):
+               console.log("You win, paper beats rock");
+                break;
+            case "scissors","paper": 
+                console.log("You win, scissors beats paper");
+                break;
+            case "rock","scissors": 
+                console.log("You win,rock beats scissors");
+                break;
+
+            default : 
+                console.log("No wayyyy,it's a draw!");
+        }
+    }
+    
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
+
+
+
+
+
+
+
+
+
+
+
